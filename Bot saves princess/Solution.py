bd4a77bec1c315f691b310e49a6f05872aa6c7f1
insivam <88207216+insivam@users.noreply.github.com>
2022-06-26 05:02:48 -0300

    optimized solution

--- Bot saves princess/Solution.py
+++ Bot saves princess/Solution.py
@@ -1,28 +1,26 @@
+from math import floor
+
+
 def displayPathtoPrincess(n, grid):
-    mario = []
-    peach = []
+    mario, peach, solution = [floor(n/2), floor(n/2)], [], []
 
     for c, i in enumerate(grid):
         if 'p' in i:
             peach.append(c)
             peach.append(i.index('p'))
-        if 'm' in i:
-            mario.append(c)
-            mario.append(i.index('m'))
-
-    if mario[1] < peach[1]:
-        for i in range(peach[1]-mario[1]):
-            print("RIGHT")
-    elif mario[1] > peach[1]:
-        for i in range(mario[1]-peach[1]):
-            print("LEFT")
 
     if mario[0] < peach[0]:
-        for i in range(peach[0]-mario[0]):
-            print("DOWN")
+        solution.extend(['DOWN'] * (peach[0]-mario[0]))
     elif mario[0] > peach[0]:
-        for i in range(mario[0]-peach[0]):
-            print("UP")
+        solution.extend(['UP'] * (mario[0]-peach[0]))
+
+    if mario[1] < peach[1]:
+        solution.extend(['RIGHT'] * (peach[1]-mario[1]))
+    elif mario[1] > peach[1]:
+        solution.extend(['LEFT'] * (mario[1]-peach[1]))
+
+    for i in solution:
+        print(i)
 
 
 m = int(input())
